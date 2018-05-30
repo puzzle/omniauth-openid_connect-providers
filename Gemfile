cbbd927d8a1069ac74294cd6edfda09df72e4fe0
Sascha Rüger <rueger@puzzle.ch>
2018-05-30 17:52:32 +0200

    add puzzle provider and update Gemfile to point to finnlabs instead of jjbohn

--- Gemfile
+++ Gemfile
@@ -3,4 +3,4 @@ source 'https://rubygems.org'
 # Specify your gem's dependencies in omniauth-openid_connect-providers.gemspec
 gemspec
 
-gem 'omniauth-openid-connect', git: 'https://github.com/jjbohn/omniauth-openid-connect.git', branch: 'master'
+gem 'omniauth-openid-connect', git: 'https://github.com/finnlabs/omniauth-openid-connect.git', branch: 'master'
---
+++ lib/omniauth/openid_connect/puzzle.rb
@@ -0,0 +1,26 @@
+module OmniAuth::OpenIDConnect
+  class Puzzle < Provider
+    def host
+      config?('host') || ENV['RAILS_OMNIAUTH_HOST']
+    end
+
+    def client_options
+      opts = {
+        :scope => :openid,
+        :identifier => ENV['RAILS_OMNIAUTH_PROVIDER'],
+        :secret => ENV["RAILS_OMNIAUTH_SECRET_KEY"],
+        :redirect_uri => ENV["RAILS_OMNIAUTH_REDIRECT_URI"],
+        :authorization_endpoint => ENV['RAILS_OMNIAUTH_AUTHORIZATION_ENDPOINT'],
+        :token_endpoint => ENV['RAILS_OMNIAUTH_TOKEN_ENDPOINT'],
+        :userinfo_endpoint => ENV['RAILS_OMNIAUTH_USERINFO_ENDPOINT'] 
+        :display_name => ENV["RAILS_OMNIAUTH_DISPLAY_NAME"],
+        :sso => ENV['RAILS_OMNIAUTH_SSO'],
+        :issuer => ENV['RAILS_OMNIAUTH_ISSUER'], 
+        :end_session_endpoint => ENV['RAILS_OMNIAUTH_END_SESSION_ENDPOINT'],
+        :check_session_iframe => ENV['RAILS_OMNIAUTH_CHECK_SESSION_IFRAME']
+      }
+
+      opts.merge super
+    end
+  end
+end
